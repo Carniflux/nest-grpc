92b62dad467b0fb2e726b12aad5901b318e1f609
Carniflux <slavazaichkin@gmail.com>
2021-03-12 16:25:53 +0300

    pre-release

--- apps/account/src/user/controllers/user.controller.ts
+++ apps/account/src/user/controllers/user.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Inject } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
+import { GrpcMethod } from '@nestjs/microservices';
 import { User } from '../models/user.interface';
-import { Observable, of, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { UserDto } from '@account/user/models/user.dto';
-import { filter, map, mergeMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { UserSubscriber } from '@account/user/services/user.subscriber';
 
 @Controller()
@@ -20,34 +20,28 @@ export class UserController {
     this.userService.update(user);
   }
 
-  @GrpcStreamMethod('EditUser', 'stream')
-  stream(message$: Observable<User>): Observable<UserDto> {
-    const userDto = new UserDto();
+  @GrpcMethod('EditUser', 'stream')
+  stream({ id }: User): Observable<UserDto> {
     const user$ = new Subject<UserDto>();
     const sub$ = new Observable<UserDto>((subscriber) => {
       this.userSubscriber.addSubscriber(subscriber);
-      message$.pipe(map((userId) => (userDto.id = userId.id)));
     }).pipe(
-      map((user) => user$.next(filter<UserDto>(() => user.id === userDto.id))),
+      map((user) => {
+        if (user.id === id) {
+          user$.next(user);
+        }
+      }),
     );
 
-    const getUser$ = of(message$).pipe(
-      mergeMap((obs) =>
-        obs.pipe(
-          mergeMap((user) =>
-            this.userService.stream(user.id).pipe(
-              map((userEn) => {
-                const userDto = new UserDto();
-                userDto.id = userEn.id;
-                userDto.name = userEn.name;
-                userDto.password = userEn.password;
-                userDto.email = userEn.email;
-                user$.next(userDto);
-              }),
-            ),
-          ),
-        ),
-      ),
+    const getUser$ = this.userService.stream(id).pipe(
+      map((userEn) => {
+        const userDto = new UserDto();
+        userDto.id = userEn.id;
+        userDto.name = userEn.name;
+        userDto.password = userEn.password;
+        userDto.email = userEn.email;
+        user$.next(userDto);
+      }),
     );
     getUser$.subscribe();
     sub$.subscribe();
